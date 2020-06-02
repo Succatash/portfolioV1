48a46848f873055509c0dd3fab5af1d6788e143e
Succatash <mstein1284@gmail.com>
2020-06-02 14:07:28 -0400

    added random color generator

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 let cards = document.querySelectorAll(".card");
+let textOverCard = document.querySelectorAll(".textOverCard");
+
 console.log(cards);
-cards.forEach((el) =>
-  el.addEventListener("click", () => console.log("clicked"))
-);
+cards.forEach((el) => el.addEventListener("mouseover", convertCards));
 
 // for (let i = 0; i < cards.length; i++) {
 //   cards[i].addEventListener("click", () => console.log("clicked"));
 // }
+
+function convertCards() {
+	this.style.backgroundColor = getRandomColor();
+	textOverCard.style.color = "purple";
+	this.innerHTML = "Hello Test Test Test";
+	console.log("mousedOver");
+}
+
+// TODO:add the random color generator
+function getRandomColor() {
+	const r = Math.floor(Math.random() * 256);
+	const g = Math.floor(Math.random() * 256);
+	const b = Math.floor(Math.random() * 256);
+	const o = Math.random() * 2;
+	// return `rgba(${r}, ${g}, ${b}, ${o})`;
+	return "rgb(" + r + ", " + g + ", " + b + ")";
+}
