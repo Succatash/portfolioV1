4a15d78768a67e78a55921a6b4a90831047dd8fe
Succatash <mstein1284@gmail.com>
2020-06-06 03:08:22 -0400

    first major commit figure out how to mouseover and iterate through a forEach loop just took some reading!!!!!!

--- app.js
+++ app.js
@@ -2,37 +2,42 @@ let cards = document.querySelectorAll(".card");
 const randomColor = getRandomColor();
 const image_x = document.querySelectorAll(".image");
 
-cards.forEach((el) =>
-  el.addEventListener("mouseover", function convertCards() {
-    el.style.backgroundColor = getRandomColor();
-    console.log("click");
-    image_x[el].style.display = "none";
-  })
+cards.forEach((el, index) =>
+	el.addEventListener("mouseover", function convertCards() {
+		el.style.backgroundColor = getRandomColor();
+		console.log("click");
+
+		image_x[index].style.display = "none";
+		console.log("works");
+	})
 );
 
-cards.forEach((el) =>
-  el.addEventListener("mouseout", () => (image_x.style.display = "initial"))
+cards.forEach((el, index) =>
+	el.addEventListener(
+		"mouseout",
+		() => (image_x[index].style.display = "initial")
+	)
 );
 
 // for (let i = 0; i < cards.length; i++) {
 //   cards[i].addEventListener("click", () => console.log("clicked"));
 // }
 
-// function convertCards() {
-//   this.style.backgroundColor = getRandomColor();
-//   console.log("click");
-//   image_x[this(el)].style.display = "none";
-// }
+function convertCards() {
+	this.style.backgroundColor = getRandomColor();
+	console.log("click");
+	image_x[index].style.display = "none";
+}
 
 // TODO:add the random color generator
 
 function getRandomColor() {
-  const r = Math.floor(Math.random() * 256);
-  const g = Math.floor(Math.random() * 256);
-  const b = Math.floor(Math.random() * 256);
-  const o = Math.random() * 2;
-  return `rgba(${r}, ${g}, ${b})`;
-  // return "rgb(" + r + ", " + g + ", " + b + ")";
+	const r = Math.floor(Math.random() * 256);
+	const g = Math.floor(Math.random() * 256);
+	const b = Math.floor(Math.random() * 256);
+	const o = Math.random() * 2;
+	return `rgba(${r}, ${g}, ${b})`;
+	// return "rgb(" + r + ", " + g + ", " + b + ")";
 }
 
 function removeImg() {}
