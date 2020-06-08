400a67bd6f000b1caf91e248a2b4366e5779bbf3
Succatash <mstein1284@gmail.com>
2020-06-08 11:34:20 -0400

    fixing the inner span

--- app.js
+++ app.js
@@ -3,19 +3,20 @@ const randomColor = getRandomColor();
 const image_x = document.querySelectorAll(".image");
 
 cards.forEach((el, index) =>
-  el.addEventListener("mouseover", function convertCards() {
-    el.style.backgroundColor = getRandomColor();
-    console.log("click");
-    image_x[index].style.display = "none";
-    console.log("works");
-  })
+	el.addEventListener("mouseover", function convertCards() {
+		el.style.backgroundColor = getRandomColor();
+		console.log("click");
+		el.style.transition = "500ms";
+		image_x[index].style.display = "none";
+		console.log("works");
+	})
 );
 
 cards.forEach((el, index) =>
-  el.addEventListener(
-    "mouseout",
-    () => (image_x[index].style.display = "initial")
-  )
+	el.addEventListener(
+		"mouseout",
+		() => (image_x[index].style.display = "initial")
+	)
 );
 
 // for (let i = 0; i < cards.length; i++) {
@@ -25,12 +26,12 @@ cards.forEach((el, index) =>
 // TODO:add the random color generator
 
 function getRandomColor() {
-  const r = Math.floor(Math.random() * 256);
-  const g = Math.floor(Math.random() * 256);
-  const b = Math.floor(Math.random() * 256);
-  const o = Math.random() * 2;
-  return `rgba(${r}, ${g}, ${b})`;
-  // return "rgb(" + r + ", " + g + ", " + b + ")";
+	const r = Math.floor(Math.random() * 256);
+	const g = Math.floor(Math.random() * 256);
+	const b = Math.floor(Math.random() * 256);
+	const o = Math.random() * 2;
+	return `rgba(${r}, ${g}, ${b})`;
+	// return "rgb(" + r + ", " + g + ", " + b + ")";
 }
 
 function removeImg() {}
